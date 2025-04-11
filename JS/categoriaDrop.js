5b75d9eb994ec5f765091a998881537faf617ea4
Sabrinabreu <168127664+Sabrinabreu@users.noreply.github.com>
2025-04-10 22:17:46 -0300

    Add files via upload

--- JS/categoriaDrop.js
+++ JS/categoriaDrop.js
@@ -1,13 +1,13 @@
-// Seleciona todas as bolinhas
 const bolinhas = document.querySelectorAll('.bolinha');
 
 // Adiciona um evento de clique para cada bolinha
 bolinhas.forEach(bolinha => {
     bolinha.addEventListener('click', function() {
-        // Remove a classe 'active' de todas as bolinhas
-        bolinhas.forEach(b => b.classList.remove('active'));
-        
-        // Adiciona a classe 'active' na bolinha clicada
-        this.classList.add('active');
+        if (this.classList.contains('active')) {
+            this.classList.remove('active');
+        } else {
+            bolinhas.forEach(b => b.classList.remove('active'));
+            this.classList.add('active');
+        }
     });
 });
